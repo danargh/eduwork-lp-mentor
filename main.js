0dfb2bfdcfd0acc94573e00643d5a7a458f5fe16
danargh <danargh86@gmail.com>
2023-10-09 15:22:10 +0700

    fix: finishing ui

--- main.js
+++ main.js
@@ -1,46 +1,3 @@
-var textDisplay = document.getElementById("text");
-var pharses = ["tanya dong kak "];
-let i = 0;
-let j = 0;
-let currentPharse = [];
-let isDeleting = false;
-let isEnd = false;
-function loop() {
-   isEnd = false;
-   textDisplay.innerHTML = currentPharse.join("");
-
-   if (i < pharses.length) {
-      if (!isDeleting && j <= pharses[i].length) {
-         currentPharse.push(pharses[i][j]);
-         j++;
-      }
-
-      if (isDeleting && j <= pharses[i].length) {
-         currentPharse.pop(pharses[i][j]);
-         j--;
-      }
-
-      if (j == pharses[i].length) {
-         isEnd = true;
-         isDeleting = true;
-      }
-
-      if (isDeleting && j === 0) {
-         currentPharse = [];
-         isDeleting = false;
-         i++;
-         if (i == pharses.length) {
-            i = 0;
-         }
-      }
-   }
-   const spedUp = Math.random() * (80 - 50) + 50;
-   const normalSpeed = Math.random() * (300 - 200) + 200;
-   const time = isEnd ? 200 : isDeleting ? spedUp : normalSpeed;
-   setTimeout(loop, time);
-}
-
-loop();
 let label = document.querySelectorAll(".acc");
 
 label.forEach((e) => {
@@ -63,7 +20,7 @@ window.onscroll = function () {
    if (prevScrollpos > currentScrollPos) {
       document.getElementById("navbar").style.top = "0";
    } else {
-      document.getElementById("navbar").style.top = "-80px";
+      document.getElementById("navbar").style.top = "-84px";
    }
    prevScrollpos = currentScrollPos;
 };
@@ -74,7 +31,7 @@ window.addEventListener("scroll", function () {
       document.getElementById("navbar").style.boxShadow =
          "0 0 10px rgba(0,0,0,0.1)";
    } else {
-      document.getElementById("navbar").style.backgroundColor = "transparent";
+      document.getElementById("navbar").style.backgroundColor = "#ffffff";
       document.getElementById("navbar").style.boxShadow = "none";
    }
 });
